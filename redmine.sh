60f9bf05a2d442b8a72dbb368631a443bd0af3a0
Bui The Phong <53887499+phongbui83@users.noreply.github.com>
2020-01-10 15:18:38 +0700

    Update redmine.sh

--- redmine.sh
+++ redmine.sh
@@ -1,7 +1,9 @@
+read -p "Nhap port Redmine ban muon mo: " RMPORT
 docker pull buithephong/cresco-redmine:v2
-docker run -it --privileged -p 80:80 -p 2222:22 buithephong/cresco-redmine:v2 --name redmine
+docker run -it --privileged -p $RMPORT:80 -p 2222:22 buithephong/cresco-redmine:v2 --name redmine
 sleep 30
 DKID=$(docker ps | grep redmine | awk '{print $1}')
-echo "hay go lenh ./start.sh"
-sleep 10
+echo "hay go lenh ./start.sh && exit"
+sleep 30
 docker exec -it $DKID bash
+echo "Truy cap http://$(hostname -i):$RMPORT username : phongbt pass: bcbcbcbc"
